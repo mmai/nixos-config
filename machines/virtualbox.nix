a619951899001218096c1a2e756e60de9441515e
Henri Bourcereau <henri.bourcereau@gmail.com>
2019-01-21 14:34:02 +0100

    vbox hardware config

--- machines/virtualbox.nix
+++ machines/virtualbox.nix
@@ -1,3 +1,12 @@
 { config, lib, pkgs, ... }:
 {
+    fileSystems."/" = {
+      device = "/dev/disk/by-label/nixos";
+      autoResize = true;
+    };
+
+    boot.growPartition = true;
+    boot.loader.grub.device = "/dev/sda";
+
+    virtualisation.virtualbox.guest.enable = true;
 }
