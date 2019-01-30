aaf31e112c0f1aa80defaabe3e55531a19dac1b7
Henri Bourcereau <henri.bourcereau@gmail.com>
2019-01-30 16:56:29 +0100

    fix base-desktop

---
+++ cfg/base-desktop.nix
@@ -0,0 +1,36 @@
+{ config, lib, pkgs, ... }:
+{
+  imports = [ 
+    ./base-minimal.nix 
+  ];
+
+  environment.systemPackages = with pkgs; [
+    gnome3.gnome-tweaks
+
+    libsForQt5.vlc # video viewer
+    zathura # pdf viewer (okular, evince ?)
+    libreoffice
+    firefox
+    chromium
+    transmission-gtk # transmission
+
+    thunderbird
+    virtualbox
+
+    gimp
+
+    gnome3.meld
+    keepassx # keepasx2 keepassxc
+    filezilla
+
+    anki # 2.0.52
+    calibre
+
+    # missing : rocketchat, teamviewer, gpick
+  ];
+
+  services.xserver.enable = true;
+  services.xserver.desktopManager.gnome3.enable = true;
+  services.xserver.displayManager.lightdm.enable = true;
+
+}
