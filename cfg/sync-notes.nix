f2b7f9cae4d18b824f04c88f8f55e03560644d99
Henri Bourcereau <henri.bourcereau@gmail.com>
2020-01-06 19:02:51 +0100

    sync-notes

---
+++ cfg/sync-notes.nix
@@ -0,0 +1,19 @@
+{ config, lib, pkgs, ... }:
+{
+ systemd.user.timers.sync-notes = {
+   description = "run sync-notes every 15 minutes";
+   wantedBy = [ "timers.target" ]; # enable it & auto start it
+  
+   timerConfig = {
+     OnBootSec = "5m"; # first run 5min after boot up
+     OnUnitInactiveSec = "15m"; # run 15min after sync-notes has finished
+   };
+ };
+
+ systemd.user.services.sync-notes = {
+   description = "Synchronize wiki and todo files with git-sync";
+   script = "cd ~/think && ${pkgs.gitAndTools.git-sync}/bin/git-sync";
+   serviceConfig.Type = "oneshot";
+ };
+
+}
