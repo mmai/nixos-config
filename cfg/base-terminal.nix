ef987a7f47654f80ecfaf03324ef0c1989734c9b
Henri Bourcereau <henri.bourcereau@gmail.com>
2022-02-25 09:01:21 +0100

    add just (better make)

--- cfg/base-terminal.nix
+++ cfg/base-terminal.nix
@@ -97,6 +97,7 @@
     gitAndTools.git-annex # sync large files with git
     gitAndTools.delta # better git diff
     gnumake
+    just # better make 
     tldr # Simplified and community-driven man pages
 
   ];
