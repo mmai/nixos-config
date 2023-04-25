af57682bce2e3e0d39478df477010e11f68231ad
Henri Bourcereau <henri.bourcereau@gmail.com>
2023-04-25 09:56:30 +0200

    teams

--- cfg/notRaspberry.nix
+++ cfg/notRaspberry.nix
@@ -5,10 +5,10 @@
 
   environment.systemPackages = with pkgs; [
     # Microsoft teams instant messaging
-    # teams # 2022-02-26 => error: cannot download teams_1.5.00.23861_amd64.deb from any mirror
+    teams
 
     # Java / Android dev
-    android-studio # launch with `unset GDK_PIXBUF_MODULE_FILE ; android-studio` (cf. https://github.com/NixOS/nixpkgs/issues/52302#issuecomment-477818365) -> done in .zsh/aliases.sh
+    # android-studio # launch with `unset GDK_PIXBUF_MODULE_FILE ; android-studio` (cf. https://github.com/NixOS/nixpkgs/issues/52302#issuecomment-477818365) -> done in .zsh/aliases.sh
     # jetbrains.jdk
     # oraclejdk # old insecure ffmpeg lib dependency 
   ];
