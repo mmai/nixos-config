d4d75510300c33f1b5f713c9499e5c7d1a8a32ec
Henri Bourcereau <henri.bourcereau@gmail.com>
2023-12-03 09:03:44 +0100

    visidata

--- cfg/atixnet.nix
+++ cfg/atixnet.nix
@@ -1,8 +1,6 @@
 { config, lib, pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    # brlaser # imprimante Brother bureaux Bordeaux
-
     # Microsoft file sharing service
     # https://github.com/abraunegg/onedrive/blob/master/docs/USAGE.md
     # unstable.onedrive
