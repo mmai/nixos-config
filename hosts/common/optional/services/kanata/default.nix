55f07a1de51b63385dd28144feee1d13dfae5635
Henri Bourcereau <henri.bourcereau@gmail.com>
2024-11-16 23:09:01 +0100

    kanata : add thinkpad integrated keyboard

--- hosts/common/optional/services/kanata/default.nix
+++ hosts/common/optional/services/kanata/default.nix
@@ -3,9 +3,15 @@
   services.kanata = {
     enable = true;
 
-    keyboards.msCurve = {
-      devices = [ "/dev/input/by-id/usb-Microsoft_Comfort_Curve_Keyboard_2000-event-kbd" ];
-      config = builtins.readFile (./atomic + "/kanata_merged.kbd");
+    keyboards = {
+      thinkpad = {
+        devices = [ "/dev/input/by-path/platform-i8042-serio-0-event-kbd" ];
+        config = builtins.readFile (./atomic + "/kanata_merged.kbd");
+      };
+      msCurve = {
+        devices = [ "/dev/input/by-id/usb-Microsoft_Comfort_Curve_Keyboard_2000-event-kbd" ];
+        config = builtins.readFile (./atomic + "/kanata_merged.kbd");
+      };
     };
   };
 }
